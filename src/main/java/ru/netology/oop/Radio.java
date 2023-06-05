b95c4a72028a5a491453abbcd8e1591fc0cc312b
Ramzesito <roman_shv@mail.ru>
2023-06-05 21:02:18 +0300

    Lombok trying (02)

--- src/main/java/ru/netology/oop/Radio.java
+++ src/main/java/ru/netology/oop/Radio.java
@@ -2,17 +2,46 @@ package ru.netology.oop;
 
 import lombok.*;
 
-
+@NoArgsConstructor(force = true)
 @RequiredArgsConstructor
-@Getter
-@Setter
-
 public class Radio {
-    private int currentVolume = 0; // from 0 to 100
-    private int currentStation = 0; // from 0 to stationQty-1
-    private int stationQty = 0; // Station quantity by default
+    @Getter private int currentVolume = 0; // from 0 to 100
+    @Getter private int currentStation = 0; // from 0 to stationQty-1
+    @Getter private final int stationQty; // Station quantity by default (will be =0 with lombok)
 
+    public void setStation(int station) {
+        if ((station >= 0) & (station <= 9)) {
+            currentStation = station;
+        }
+    }
 
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
 
 
 
